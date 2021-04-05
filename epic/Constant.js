4662fd75dc1e109c06af58c0c4ef2ec6fcb30dc9
Pol-Antoine Loiseau <Lord-Bledard@linux.fritz.box>
2021-04-05 17:02:38 -0400

    [Fix] : Folder name

--- epic/Constant.js
+++
@@ -1,9 +0,0 @@
-export const CLIENT_ID = 'a035632d95e7793';
-
-export const DISCOVERY = {
-  authorizationEndpoint: 'https://api.imgur.com/oauth2/authorize',
-  tokenEndpoint: 'https://api.imgur.com/oauth2/token',
-};
-
-export var ACCESS_TOKEN = null;
-
